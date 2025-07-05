5cfa3ebbd738c049f643240339da0685ca9b2911
reem-elbana <reem.elbana123@gmail.com>
2025-07-05 22:01:30 +0300

    Update Layout.jsx

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
-import Footer from "../footer/Footer";
+import Footer from "../Footer/Footer";
 
 const Layout = ({ onSearch }) => {
   return (
